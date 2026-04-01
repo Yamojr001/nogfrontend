e0e4702d5eb20f59d5f19a90d3b8d7c73dd45653
Yamojr001 <yamojr001@gmail.com>
2026-04-01 15:09:18 +0100

    fixing build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
     visible: {
       y: 0,
       opacity: 1,
-      transition: { duration: 0.8, ease: [0.16, 1, 0.3, 1] }
+      transition: { duration: 0.8, ease: "easeOut" }
     }
   };
 
